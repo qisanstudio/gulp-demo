352717a61ede38967b8e7cbdbe014688e5f78f5f
qisanstudio <qisanstudio@gmail.com>
2014-08-22 18:39:28 +0800

    demo

---
+++ app/scripts/test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+require(['bootstrapx', function(bs) {
+    console.log(bs);
+}]);
---
+++ dist/base.js
@@ -0,0 +1,12 @@
+(function () {
+require.config({
+    paths: {
+        'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap'
+    },
+    shim: {
+    
+    }
+});
+
+define("config", function(){});
+}());
